89d4b3a125adb0e689a6bc1e33e07c96e9b715d2
PouPeuu <qwertydude277@gmail.com>
2023-05-01 12:16:45 +0300

    Added Piss

---
+++ weapons/Piss/Piss.lua
@@ -0,0 +1,25 @@
+required_objects = {}
+
+max_energy = 1
+req_energy = 1
+req_energy_level = 1
+delay = 0
+recoil = 0
+speed = 100
+spread = 5
+
+math.randomseed(os.time())
+
+function piss(dx,dy)
+  create_object("particle", 0, 0, dx*speed+math.random(-spread,spread), dy*speed+math.random(-spread,spread), 232, 217, 0, 2, false, 5, 0.5)
+end
+
+function launch()
+  dir = get_platform_dir()
+  dx, dy = get_dir_vector(dir)
+  x, y = get_platform_pos()
+
+  piss(dx,dy)
+  piss(dx,dy)
+  piss(dx,dy)
+end
--- weapons/SuperLaserPiss/SuperLaserPiss.lua
+++ weapons/SuperLaserPiss/SuperLaserPiss.lua
@@ -4,7 +4,7 @@ max_energy = 1
 req_energy = 1
 req_energy_level = 1
 delay = 0
-recoil = 2000
+recoil = 5000
 speed = 200
 
 function launch()
@@ -15,6 +15,6 @@ function launch()
   hitX, hitY = find_first_solid_point(x,y,x+dx*1000,y+dy*1000)
   send_game_message("dx: "..dx.." dy: "..dy.." hitX: "..hitX.." hitY: "..hitY)
   --id = create_object("particle",hitX-x,hitY-y,0,0,255,255,255,5,true,0.1,0)
-  id = create_object("small_explosion", hitX-x, hitY-y, 0, 0)
-  create_object("rope",1,0.05,get_platform_id(),id)
+  id = create_object("explosion3", hitX-x, hitY-y, 0, 0)
+  create_object("rope",1,0.1,get_platform_id(),id)
 end
